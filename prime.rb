4fcfeb84b9a6872becd30d7b163277ee6a715525
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-16 16:02:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(num)
+  divisor = 0
+  if num == 1
+    return false
+  elsif num > 0
+    array =* (2...(num-1))
+    array.each {|x| num % x == 0 ? divisor = x : nil}
+    if divisor != 0
+      return false
+    elsif divisor == 0
+      return true
+    end
+  elsif num<0
+    return false
+  else
+    return false
+  end
+end
